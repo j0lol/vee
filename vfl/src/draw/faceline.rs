65f7fd5ee00bd5f52875a43c7c2f68d035ed0b2a
j0 <me@j0.lol>
2025-05-25 13:48:29 +0100

    minor cleanup in vfl

--- vfl/src/draw/faceline.rs
+++ vfl/src/draw/faceline.rs
@@ -4,22 +4,22 @@ pub fn trivial_quad() -> (Vec<Vertex>, Vec<u32>) {
     (
         vec![
             Vertex {
-                position: [1.0 - 0.5, 0.0 - 0.5, 0.0],
+                position: [0.5, -0.5, 0.0],
                 tex_coords: [0.0, 0.0],
                 normal: [0.0, 0.0, 0.0],
             },
             Vertex {
-                position: [1.0 - 0.5, 1.0 - 0.5, 0.0],
+                position: [0.5, 0.5, 0.0],
                 tex_coords: [0.0, 1.0],
                 normal: [0.0, 0.0, 0.0],
             },
             Vertex {
-                position: [0.0 - 0.5, 1.0 - 0.5, 0.0],
+                position: [-0.5, 0.5, 0.0],
                 tex_coords: [1.0, 1.0],
                 normal: [0.0, 0.0, 0.0],
             },
             Vertex {
-                position: [0.0 - 0.5, 0.0 - 0.5, 0.0],
+                position: [-0.5, -0.5, 0.0],
                 tex_coords: [1.0, 0.0],
                 normal: [0.0, 0.0, 0.0],
             },
