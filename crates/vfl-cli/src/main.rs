43b035d9bfcf4ffbc6cea9d13c82ddeff1fc54dd
j0 <me@j0.lol>
2025-05-29 20:02:43 +0100

    add vfl-cli

--- crates/vfl-cli/src/main.rs
+++ crates/vfl-cli/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
 use vfl::charinfo::nx::BinRead;
+use vfl::res::tex::nx::{ResourceTexture, TextureElement};
 
 // TODO: use real names
 // https://github.com/ariankordi/ffl/blob/97eecdf3688f92c4c95cecf5d6ab3e84c0ee42c0/tools/FFLResource.py#L448
@@ -41,6 +42,14 @@ enum Subcommands {
         index: usize,
         output: PathBuf,
     },
+
+    /// Show existant textures
+    TextureExists {
+        #[arg(short, long)]
+        resource_file: PathBuf,
+        #[arg(value_enum, short, long)]
+        texture_type: TextureType,
+    },
 }
 
 // This is kind of `clap`-slop. I just need a quick debug tool.
@@ -62,21 +71,76 @@ fn main() {
             let res_file = std::fs::read(resource_file).unwrap();
 
             // *YandereDev Voice* If only there was a better way...
-            let texture_element = match texture_type {
-                TextureType::Hat => res_tex.hat[index],
-                TextureType::Eye => res_tex.eye[index],
-                TextureType::Eyebrow => res_tex.eyebrow[index],
-                TextureType::Beard => res_tex.beard[index],
-                TextureType::Wrinkle => res_tex.wrinkle[index],
-                TextureType::Makeup => res_tex.makeup[index],
-                TextureType::Glass => res_tex.glass[index],
-                TextureType::Mole => res_tex.mole[index],
-                TextureType::Mouth => res_tex.mouth[index],
-                TextureType::Mustache => res_tex.mustache[index],
-                TextureType::NoseLine => res_tex.noseline[index],
-            };
+            let texture_element = lookup_texture_type(texture_type, index, res_tex).unwrap();
             let el = texture_element.get_image(&res_file).unwrap().unwrap();
             el.save(output).unwrap();
         }
+        Subcommands::TextureExists {
+            resource_file,
+            texture_type,
+        } => {
+            let res_tex = ResourceTexture::read(&mut BufReader::new(
+                File::open(resource_file.clone()).unwrap(),
+            ))
+            .unwrap();
+            let res_file = std::fs::read(resource_file).unwrap();
+
+            let mut exists = vec![];
+            for index in 0..match texture_type {
+                TextureType::Hat => res_tex.hat.len(),
+                TextureType::Eye => res_tex.eye.len(),
+                TextureType::Eyebrow => res_tex.eyebrow.len(),
+                TextureType::Beard => res_tex.beard.len(),
+                TextureType::Wrinkle => res_tex.wrinkle.len(),
+                TextureType::Makeup => res_tex.makeup.len(),
+                TextureType::Glass => res_tex.glass.len(),
+                TextureType::Mole => res_tex.mole.len(),
+                TextureType::Mouth => res_tex.mouth.len(),
+                TextureType::Mustache => res_tex.mustache.len(),
+                TextureType::NoseLine => res_tex.noseline.len(),
+            } {
+                let texture_element = lookup_texture_type(texture_type, index, res_tex).unwrap();
+                if (texture_element.texture.width == 0 && texture_element.texture.height == 0)
+                    || (texture_element.texture.width == 8 && texture_element.texture.height == 8)
+                {
+                    exists.push(false);
+                    continue;
+                }
+                let texture = texture_element.get_image(&res_file).unwrap();
+
+                let valid = texture.is_some();
+
+                exists.push(valid);
+            }
+
+            println!(
+                "{:#?}",
+                exists
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(num, bool)| if *bool { Some(num) } else { None })
+                    .collect::<Vec<_>>()
+            )
+        }
+    }
+}
+
+fn lookup_texture_type(
+    texture_type: TextureType,
+    index: usize,
+    res_tex: ResourceTexture,
+) -> Option<TextureElement> {
+    match texture_type {
+        TextureType::Hat => res_tex.hat.get(index).copied(),
+        TextureType::Eye => res_tex.eye.get(index).copied(),
+        TextureType::Eyebrow => res_tex.eyebrow.get(index).copied(),
+        TextureType::Beard => res_tex.beard.get(index).copied(),
+        TextureType::Wrinkle => res_tex.wrinkle.get(index).copied(),
+        TextureType::Makeup => res_tex.makeup.get(index).copied(),
+        TextureType::Glass => res_tex.glass.get(index).copied(),
+        TextureType::Mole => res_tex.mole.get(index).copied(),
+        TextureType::Mouth => res_tex.mouth.get(index).copied(),
+        TextureType::Mustache => res_tex.mustache.get(index).copied(),
+        TextureType::NoseLine => res_tex.noseline.get(index).copied(),
     }
 }
